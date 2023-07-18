fe19b1683cccc6e52a598bc0aa5d82a0d3112ba0
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 17:37:03 -0700

    Add main loop of game and tweak gamelogic skeleton

--- gamelogic.rb
+++ gamelogic.rb
@@ -3,7 +3,12 @@
 ##
 # Represents the core logic of the game.
 class GameLogic
-  def initialize
+  attr_accessor :tries
+
+  def initialize(tries)
+    raise ArgumentError, 'Argument should be greater than 0.' unless tries.positive?
+
+    @tries = tries
     codemaker_type = query_codemaker_type
     codebreaker_type = query_codebreaker_type
 
@@ -18,8 +23,27 @@ class GameLogic
   def query_codebreaker_type
   end
 
+  # Prints codebreaker victory message to console.
+  def print_codebreaker_win(guesses) end
+
+  # Prints codemaker victory message to console.
+  def print_codemaker_win
+  end
+
   # Runs main game loop.
   def gameloop(codemaker_type, codebreaker_type)
+    @codemaker = codemaker_type == 'AI' ? CodeMakerAI.new : CodeMaker.new
+    @codebreaker = codebreaker_type == 'AI' ? CodeBreakerAI.new : CodeBreaker.new
+
+    @password = @codemaker.query_password
+    @guess = Guess.new
+
+    1..@tries.each do |guess|
+      @guess = @codebreaker.query_guess
+      return print_codebreaker_win(guess) if @password.correct_guess?(@guess)
+    end
+
+    print_codemaker_win
   end
 end
 
