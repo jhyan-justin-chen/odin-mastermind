95a1290b83cdd4f0920c4b1c09910ac21ae589c0
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 20:10:13 -0700

    Fix various bugs in gamelogic
    Class variables not properly formatted
    
    Missing arguments
    
    Missing parentheses

--- gamelogic.rb
+++ gamelogic.rb
@@ -16,8 +16,8 @@ class GameLogic
 
     @tries = tries
     @console = Console.new
-    codemaker_type = console.query_codemaker_type until %w[AI Player].include?(codemaker_type)
-    codebreaker_type = console.query_codebreaker_type until %w[AI Player].include?(codebreaker_type)
+    codemaker_type = @console.query_codemaker_type until %w[AI Player].include?(codemaker_type)
+    codebreaker_type = @console.query_codebreaker_type until %w[AI Player].include?(codebreaker_type)
 
     gameloop(codemaker_type, codebreaker_type)
   end
@@ -30,14 +30,14 @@ class GameLogic
     @password = @codemaker.query_password
     @guess = Guess.new
 
-    1..@tries.each do |guess|
+    (1..@tries).each do |guess|
       @guess = @codebreaker.query_guess
-      @console.show_guess_hints(@guess.count_misplaced_digits, @guess.count_correct_digits)
-      return console.show_codebreaker_win(guess) if @password.correct_guess?(@guess)
+      @console.show_guess_hints(@guess.count_misplaced_digits(@password), @guess.count_correct_digits(@password))
+      return @console.show_codebreaker_win(guess) if @password.correct_guess?(@guess)
     end
 
-    console.show_codemaker_win
+    @console.show_codemaker_win
   end
 end
 
-GameLogic.new
+GameLogic.new(5)
