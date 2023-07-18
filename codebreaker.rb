3f0300101b3850d641a8bdc329ddba50d8980bf0
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 19:08:52 -0700

    Implement codebreaker

--- codebreaker.rb
+++ codebreaker.rb
@@ -3,6 +3,19 @@
 ##
 # Represents the player guessing the code.
 class CodeBreaker
+  def initialize
+    @console = Console.new
+  end
+
   # Queries for a new attempt at guessing the password.
-  def query_guess() end
+  def query_guess
+    loop do
+      @guess = Guess.new(@console.query_guess)
+    rescue ArgumentError
+      @console.inform_guess_error
+    else
+      break
+    end
+    @guess
+  end
 end
