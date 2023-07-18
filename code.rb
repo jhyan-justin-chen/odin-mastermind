9e91ecee00b8beee91dfb7a8ecb5e6db6e9b7795
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 19:05:12 -0700

    Rework code validation

--- code.rb
+++ code.rb
@@ -4,12 +4,20 @@
 # Represents a code in Mastermind.
 class Code
   def initialize(*digits)
-    @code = if digits.length == 4
-              [digits[0], digits[1], digits[2], digits[3]]
-            elsif digits.empty?
-              [nil, nil, nil, nil]
-            else
-              raise ArgumentError, 'Argument should consist of four digits'
-            end
+    raise ArgumentError, 'Invalid code' unless valid?(digits)
+
+    @code = digits.nil? ? [nil, nil, nil, nil] : [digits[0], digits[1], digits[2], digits[3]]
+  end
+
+  def valid_digits
+    [1, 2, 3, 4, 5]
+  end
+
+  def valid?(digits)
+    return true if digits.nil?
+    return false unless digits.length == 4
+    return false unless (digits - valid_digits).empty?
+
+    true
   end
 end
