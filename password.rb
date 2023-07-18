f36ad382cef33fff1ca1f3830099d14a05bfb0eb
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 19:06:14 -0700

    Implement password

--- password.rb
+++ password.rb
@@ -4,11 +4,21 @@
 # Represents the code that is chosen at the start.
 class Password < Code
   # Checks if a digit is in the correct location.
-  def correct_digit?(digit, index) end
+  def correct_digit?(digit, index)
+    @code[index] == digit
+  end
 
   # Checks if a digit exists in the password.
-  def contains_digit?(digit) end
+  def contains_digit?(digit)
+    @code.include?(digit)
+  end
 
   # Checks if the guess is fully correct.
-  def correct_guess?(guess) end
+  def correct_guess?(guess)
+    @code == guess
+  end
+
+  def nil?
+    @code.all?(&:nil?)
+  end
 end
