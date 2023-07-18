3d3dfdd0d52eb6daf127c3910482d51eb52935bf
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 17:54:22 -0700

    Add new class for managing console input/output

---
+++ console.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+##
+# Represents the console interface for Mastermind.
+class Console
+  # Prints codebreaker victory message to console.
+  def print_codebreaker_win(guesses) end
+
+  # Prints codemaker victory message to console.
+  def print_codemaker_win
+  end
+end
