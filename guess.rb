b208255e64cf40c36ec5da603412b570ccac6505
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 19:06:07 -0700

    Implement guess

--- guess.rb
+++ guess.rb
@@ -4,8 +4,12 @@
 # Represents a guess at the code.
 class Guess < Code
   # Gets the number of digits that are correct but in the wrong location.
-  def count_misplaced_digits(password) end
+  def count_misplaced_digits(password)
+    @code.select { |digit| password.contains_digit?(digit) }.length - count_correct_digits(password)
+  end
 
   # Gets the number of digits that are correct and in the right location.
-  def count_correct_digits(password) end
+  def count_correct_digits(password)
+    @code.each_with_index { |digit, index| password.correct_digit?(digit, index) }
+  end
 end
