0c793125e9988b1ca192f5d730425aa747498b12
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-07-17 19:05:28 -0700

    Implement codemaker

--- codemaker.rb
+++ codemaker.rb
@@ -3,6 +3,19 @@
 ##
 # Represents the player making the code to be guessed.
 class CodeMaker
+  def initialize
+    @console = Console.new
+  end
+
   # Queries for a new password.
-  def query_password() end
+  def query_password
+    loop do
+      @password = Password.new(@console.query_password)
+    rescue ArgumentError
+      @console.inform_password_error
+    else
+      break
+    end
+    @password
+  end
 end
